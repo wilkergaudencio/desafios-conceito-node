b30217d1e76a99228d4c96165f60ea3024f32321
Wilker <wilker.gaudencio@riachuelo.com.br>
2020-04-08 13:51:50 -0300

    node conceito

--- src/app.js
+++ src/app.js
@@ -1,33 +1,85 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/repositories/:id', validateProjectId);
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  const repoExists = repositories.find(repo => repo.id === id);
+
+  if (!repoExists) {
+    return res.status(400).json({ error: 'Repository not found' });
+  }
+
+  req.repo = repoExists;
+
+  return next();
+}
+
+app.get("/repositories", (req, res) => {
+  return res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repoExists = repositories.find(repo => repo.url === url);
+
+  if (repoExists) {
+    return res.json(repoExists);
+  }
+
+  const newRepo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(newRepo);
+
+  return res.json(newRepo);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  const { repo } = req;
+  const { title, url, techs } = req.body;
+
+  repo.title = title;
+  repo.url = url;
+  repo.techs = techs;
+
+  return res.json(repo);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  repositories.splice(repoIndex, 1);
+
+  return res.status(204).json({});
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (req, res) => {
+  const { repo } = req;
+
+  repo.likes = repo.likes + 1;
+
+  return res.json({
+    likes: repo.likes,
+  });
 });
 
 module.exports = app;
